1ef9eb7ba0dc13fcb45f9df3bb162aba345ea3a3
chase <chase@bryx.com>
2015-09-24 11:06:31 -0400

    Added a class for testing the parsing of CRS Objects

---
+++ src/test/kotlin/crs/CrsTest.kt
@@ -0,0 +1,58 @@
+package crs
+
+import edu.csh.chase.kgeojson.GeoJson
+import edu.csh.chase.kgeojson.coordinatereferencesystem.CoordinateReferenceSystemType
+import edu.csh.chase.kgeojson.coordinatereferencesystem.LinkedCoordinateReferenceSystem
+import edu.csh.chase.kgeojson.coordinatereferencesystem.NamedCoordinateReferenceSystem
+import edu.csh.chase.kjson.json
+import org.junit.Test
+import java.net.URI
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+import kotlin.test.assertTrue
+
+class CrsTest {
+
+    @Test fun testInvalidCrs() {
+        val crsObject = json(
+                "type" to "invalid",
+                "properties" to json(
+                        "name" to "CRS"
+                )
+        )
+        assertNull(GeoJson.parseCoordinateReferenceSystem(crsObject))
+    }
+
+    @Test fun testNamedCrs() {
+        val crsObject = json(
+                "type" to "name",
+                "properties" to json(
+                        "name" to "CRS"
+                )
+        )
+
+        val namedCrs = GeoJson.parseCoordinateReferenceSystem(crsObject)
+        assertNotNull(namedCrs)
+        assertTrue(namedCrs is NamedCoordinateReferenceSystem)
+        assertEquals(CoordinateReferenceSystemType.Name, namedCrs!!.type)
+        assertEquals("CRS", (namedCrs as NamedCoordinateReferenceSystem).name)
+
+    }
+
+    @Test fun testBasicLinkedCrs() {
+        val crsObject = json(
+                "type" to "link",
+                "properties" to json(
+                        "href" to "http://example.com"
+                )
+        )
+        val linkedCrs = GeoJson.parseCoordinateReferenceSystem(crsObject)
+        assertNotNull(linkedCrs)
+        assertTrue(linkedCrs is LinkedCoordinateReferenceSystem)
+        assertEquals(CoordinateReferenceSystemType.Link, linkedCrs!!.type)
+        assertEquals(URI("http://example.com"), (linkedCrs as LinkedCoordinateReferenceSystem).href)
+        assertNull(linkedCrs.linkType)
+    }
+
+}
