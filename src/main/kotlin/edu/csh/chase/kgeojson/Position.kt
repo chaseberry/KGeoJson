38c50703e0c81afc9321056526102cf98da85a1e
chase <chase@bryx.com>
2015-09-23 20:22:53 -0400

    Finalized a basic Position class

--- src/main/kotlin/edu/csh/chase/kgeojson/Position.kt
+++ src/main/kotlin/edu/csh/chase/kgeojson/Position.kt
@@ -1,3 +1,14 @@
 package edu.csh.chase.kgeojson
 
-data class Position(val x: Double, val y: Double, val z: Double, val rest: Array<Double>)
+import edu.csh.chase.kjson.JsonArray
+
+//TODO Add support for N arguments past the x, y, z
+
+data class Position(val x: Double, val y: Double, val z: Double?)
+
+fun positionFromJson(positionArray: JsonArray): Position? {
+    val x = positionArray.getDouble(0) ?: return null
+    val y = positionArray.getDouble(1) ?: return null
+    val z = positionArray.getDouble(2)
+    return Position(x, y, z)
+}
