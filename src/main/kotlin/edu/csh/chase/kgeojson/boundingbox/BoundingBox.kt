02c5517e974ec5f3ec0f7ef741707ccd57a602c4
chase <chase@bryx.com>
2015-09-24 14:16:02 -0400

    Added a contains to check if a position is in a BBox

--- src/main/kotlin/edu/csh/chase/kgeojson/boundingbox/BoundingBox.kt
+++ src/main/kotlin/edu/csh/chase/kgeojson/boundingbox/BoundingBox.kt
@@ -6,4 +6,16 @@ public data class BoundingBox(val bottomLeft: Position, val topRight: Position) 
 
     public val dimensions: Int = bottomLeft.dimensions
 
+    public fun contains(position: Position): Boolean {
+        return position.x >= bottomLeft.x &&
+                position.y >= bottomLeft.y &&
+                position.x <= topRight.x &&
+                position.y <= topRight.y &&
+                ((position.z == null ||
+                        bottomLeft.z == null ||
+                        topRight.z == null) ||
+                        position.z >= bottomLeft.z &&
+                                position.z <= topRight.z)
+    }
+
 }
