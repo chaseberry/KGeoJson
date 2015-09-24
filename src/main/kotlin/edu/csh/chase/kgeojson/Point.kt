d77d7907283d3232f1a5e76e4ec21b427d87599e
chase <chase@bryx.com>
2015-09-24 14:34:10 -0400

    Added shortcuts for x, y, z Added the properties field for constructing

--- src/main/kotlin/edu/csh/chase/kgeojson/Point.kt
+++ src/main/kotlin/edu/csh/chase/kgeojson/Point.kt
@@ -4,8 +4,14 @@ import edu.csh.chase.kgeojson.boundingbox.BoundingBox
 import edu.csh.chase.kgeojson.coordinatereferencesystem.CoordinateReferenceSystem
 
 class GeoJsonPoint(val position: Position,
+                   properties: Map<String, Any?>,
                    crs: CoordinateReferenceSystem? = null,
-                   boundingBox: BoundingBox? = null) : GeoJsonBase(GeoJsonType.Point, crs, boundingBox) {
+                   boundingBox: BoundingBox? = null) : GeoJsonBase(GeoJsonType.Point, crs, boundingBox, properties) {
 
+    val x = position.x
+
+    val y = position.y
+
+    val z = position.z
 
 }
