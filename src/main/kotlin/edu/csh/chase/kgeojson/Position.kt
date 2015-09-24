b70e682411b086f7db4c689bb08ac6f686929314
chase <chase@bryx.com>
2015-09-24 11:29:49 -0400

    Added a dimension count Added a TODO about the number of dimensions

--- src/main/kotlin/edu/csh/chase/kgeojson/Position.kt
+++ src/main/kotlin/edu/csh/chase/kgeojson/Position.kt
@@ -4,7 +4,12 @@ import edu.csh.chase.kjson.JsonArray
 
 //TODO Add support for N arguments past the x, y, z
 
-data class Position(val x: Double, val y: Double, val z: Double?)
+data class Position(val x: Double, val y: Double, val z: Double?) {
+
+    //TODO is the number of dimensions == size of the position array?
+    val dimensions: Int = if ( z == null) 2 else 3
+
+}
 
 fun positionFromJson(positionArray: JsonArray): Position? {
     val x = positionArray.getDouble(0) ?: return null
