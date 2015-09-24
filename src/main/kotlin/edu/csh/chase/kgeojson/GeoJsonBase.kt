1b0039fad61bb490df57174f0b5a402661afeba1
chase <chase@bryx.com>
2015-09-24 16:38:39 -0400

    Added a properties field for arbitrary properties Added a get function to access them

--- src/main/kotlin/edu/csh/chase/kgeojson/GeoJsonBase.kt
+++ src/main/kotlin/edu/csh/chase/kgeojson/GeoJsonBase.kt
@@ -5,7 +5,11 @@ import edu.csh.chase.kgeojson.coordinatereferencesystem.CoordinateReferenceSyste
 
 abstract class GeoJsonBase(val type: GeoJsonType,
                            val coordinateReferenceSystem: CoordinateReferenceSystem?,
-                           val boundingBox: BoundingBox?) {
+                           val boundingBox: BoundingBox?,
+                           val properties: Map<String, Any?>) {
 
+    fun get(key: String): Any? {
+        return properties[key]
+    }
 
 }
