5e780ddefc258a9d5d53aedc6f26e29ce3c17808
chase <chase@bryx.com>
2015-09-24 11:06:21 -0400

    Added a TODO for properties

--- src/main/kotlin/edu/csh/chase/kgeojson/coordinatereferencesystem/CoordinateReferenceSystem.kt
+++ src/main/kotlin/edu/csh/chase/kgeojson/coordinatereferencesystem/CoordinateReferenceSystem.kt
@@ -1,3 +1,5 @@
 package edu.csh.chase.kgeojson.coordinatereferencesystem
 
+//TODO consider keeping the rest of properties here?
+
 abstract class CoordinateReferenceSystem(val type: CoordinateReferenceSystemType)
