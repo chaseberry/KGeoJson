75ad99262fc1cee80f44e9486bdf81c28c6219a6
chase <chase@bryx.com>
2015-09-24 14:16:30 -0400

    Added a BBox? param Spaced out the constructor

--- src/main/kotlin/edu/csh/chase/kgeojson/GeoJsonBase.kt
+++ src/main/kotlin/edu/csh/chase/kgeojson/GeoJsonBase.kt
@@ -1,8 +1,11 @@
 package edu.csh.chase.kgeojson
 
+import edu.csh.chase.kgeojson.boundingbox.BoundingBox
 import edu.csh.chase.kgeojson.coordinatereferencesystem.CoordinateReferenceSystem
 
-abstract class GeoJsonBase(val type: GeoJsonType, val coordinateReferenceSystem: CoordinateReferenceSystem?) {
+abstract class GeoJsonBase(val type: GeoJsonType,
+                           val coordinateReferenceSystem: CoordinateReferenceSystem?,
+                           val boundingBox: BoundingBox?) {
 
 
 }
