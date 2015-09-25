1bc09edbe299270d731aedf9429994285763cbc2
chase <chase@bryx.com>
2015-09-25 13:10:12 -0400

    Added a json Serialize to the GeoJsonBase

--- src/main/kotlin/edu/csh/chase/kgeojson/GeoJsonBase.kt
+++ src/main/kotlin/edu/csh/chase/kgeojson/GeoJsonBase.kt
@@ -3,11 +3,16 @@ package edu.csh.chase.kgeojson
 import edu.csh.chase.kgeojson.boundingbox.BoundingBox
 import edu.csh.chase.kgeojson.coordinatereferencesystem.CoordinateReferenceSystem
 import edu.csh.chase.kjson.JsonObject
+import edu.csh.chase.kjson.JsonSerializable
 
 abstract class GeoJsonBase(val type: GeoJsonType,
                            val coordinateReferenceSystem: CoordinateReferenceSystem?,
                            val boundingBox: BoundingBox?,
-                           val properties: JsonObject) {
+                           val properties: JsonObject) : JsonSerializable {
+
+    override fun jsonSerialize(): String {
+        return properties.toString()
+    }
 
     fun get(key: String): Any? {
         return properties[key]
