156704a8adfe19a54ff87d41a6f5327b225688d1
chase <chase@bryx.com>
2015-09-26 10:28:12 -0400

    Added a MultiLineString

---
+++ src/main/kotlin/edu/csh/chase/kgeojson/MultiLineString.kt
@@ -0,0 +1,18 @@
+package edu.csh.chase.kgeojson
+
+import edu.csh.chase.kgeojson.boundingbox.BoundingBox
+import edu.csh.chase.kgeojson.coordinatereferencesystem.CoordinateReferenceSystem
+import edu.csh.chase.kjson.JsonObject
+
+class GeoJsonMultiLineString(val lines: Array<Line>,
+                             properties: JsonObject,
+                             crs: CoordinateReferenceSystem? = null,
+                             boundingBox: BoundingBox? = null) :
+        GeoJsonBase(GeoJsonType.MultiLineString, crs, boundingBox, properties),
+        Iterable<Line> {
+
+    override fun iterator(): Iterator<Line> = lines.iterator()
+
+    fun get(index: Int): Line = lines[index]
+
+}
